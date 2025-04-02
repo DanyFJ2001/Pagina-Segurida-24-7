46056b09e35c1ded514aad023fe107263162b1d8
rosita349 <quezadarosita76@gmail.com>
2025-04-02 10:34:25 -0500

    paquetes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PaquetesComponent } from './pages/paquetes/paquetes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'paquetes',
+        component:PaquetesComponent
+    },
+];
